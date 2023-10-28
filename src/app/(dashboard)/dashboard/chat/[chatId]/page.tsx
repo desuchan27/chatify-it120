e6f5cd193af8ca0451689595c6c9e3b876262a8f
Desu Chan <orenodesukouichi@gmail.com>
2023-10-28 15:56:21 +0800

    added message function (not realtime yet)

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,4 +1,7 @@
+import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { messageArrayValidator } from '@/lib/validations/message'
 import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
@@ -9,14 +12,36 @@ interface pageProps {
   }
 }
 
+async function getChatMessages(chatId: string) {
+  try {
+    const results: string[] = await fetchRedis(
+      'zrange',
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    )
+    const dbMessages = results.map((message) => JSON.parse(message) as Message) //to parse messages
+
+    //to distribute messages in reverse order
+    const reverseDbMessages = dbMessages.reverse()
+
+    //to parse the db messages
+    const messages = messageArrayValidator.parse(reverseDbMessages)
+
+    return messages
+  } catch (error) {
+    notFound()
+  }
+}
+
 const page = async ({ params }: pageProps) => {
-  
-  
-  const {chatId} = params //destructuring chatId  
+
+
+  const { chatId } = params //destructuring chatId  
   const session = await getServerSession(authOptions) //determine the session
 
   //to check if there is no session
-  if(!session) notFound()
+  if (!session) notFound()
 
   //destructure the user
   const { user } = session
@@ -24,7 +49,13 @@ const page = async ({ params }: pageProps) => {
   //access to chatId
   const [userId1, userId2] = chatId.split('--')
 
+  if (user.id !== userId1 && user.id !== userId2) {
+    notFound()
+  }
 
+  const chatPartnerId = user.id === userId1 ? userId2 : userId1 //to determine id which is yours
+  const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User //to get the chat partner info
+  const initialMessages = await getChatMessages(chatId) //to get the messages
 
   return <div>{params.chatId}</div>
 }
